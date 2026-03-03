3615f5c12325bc371181b8b79493ec2eeb519270
Finn Melzer <finn.melzer@mayflower.de>
2026-03-03 15:56:42 +0100

    feat: add nitro externals configuration for improved module tracing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   modules: ['nuxt-auth-utils', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   colorMode: { classSuffix: '' },
+  nitro: {
+    externals: {
+      traceInclude: ['node_modules/nuxt/dist/app/types/augments.js'],
+    },
+  },
   runtimeConfig: {
     databaseUrl: '',
     session: { password: '' },
